26da6eb175f9b366d4b86f2d1361a86914ecca35
Charles Chandler <charles.socrates.chandler@gmail.com>
2014-11-15 19:20:42 -0600

    commiting changes before pulling master

--- StadiumPark/StadiumDetailsViewController.m
+++ StadiumPark/StadiumDetailsViewController.m
@@ -72,8 +72,6 @@
 - (void)setLogo {
     NSLog(@"logo: %@", [self.stadiumDetails[@"stadium"] objectForKey:@"logo"]);
     if ([[self.stadiumDetails[@"stadium"] objectForKey:@"logo"]  isEqual: [NSNull null]]) {
-        UIImage *placeholderImage = [UIImage imageNamed:@"background1" inBundle:nil compatibleWithTraitCollection:nil];
-        //self.stadiumLogoImageView.image = placeholderImage;
         self.stadiumLogoImageView.hidden = YES;
     } else {
         UIImage *logo = [UIImage imageWithData:[self.stadiumDetails[@"stadium"] objectForKey:@"logo"]];
